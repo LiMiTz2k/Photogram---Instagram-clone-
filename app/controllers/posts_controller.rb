bb6c6daf63efab6ad27265ce761fa7795b11c56b
Kofi Dankwa <Kofi.dankwa1994@hotmail.com>
2016-06-15 15:50:01 +0100

    commiting changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -10,16 +11,17 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
-      flash[:success] = "Your post has been created!"
-      redirect_to posts_path
+   @post = current_user.posts.build(post_params)
+    if @post.save
+       flash[:success] = "Your post has been created!"
+       redirect_to posts_path
     else
-      flash[:alert] = "Your post couldn't be created! Please check the form."
-      render :new
+       flash[:alert] = "Your post couldn't be created! Please check the form."
+       render :new
     end
   end
 
@@ -53,6 +55,13 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 
 end
